b17c76f126568a9ea6fa3f84cde9f3409dae4970
Youngho <cyh6099@gmail.com>
2024-01-17 03:22:06 +0900

    feat: 주차장 타입 변경

--- src/main/java/com/example/parking/domain/parking/Type.java
+++ src/main/java/com/example/parking/domain/parking/Type.java
@@ -9,9 +9,9 @@ import lombok.Getter;
 
 @Getter
 public enum Type {
-    OFF_STREET("노외 주차장"),
-    ON_STREET("노상 주차장"),
-    MECHANICAL("기계식 주차장");
+    OFF_STREET("노외"),
+    ON_STREET("노상"),
+    MECHANICAL("기계식");
 
     private static final Map<String, Type> descriptions =
             Collections.unmodifiableMap(Stream.of(values())
