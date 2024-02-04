66f3a54077ac32127a78ffb793cae3bb70548bf0
JunDonghyuk <ehdgur4814@naver.com>
2024-02-04 21:05:32 +0900

    feat: Member 설계 및 기능 구현

---
+++ src/main/java/com/example/parking/domain/member/Member.java
@@ -0,0 +1,57 @@
+package com.example.parking.domain.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String nickname;
+
+    @Embedded
+    private Password password;
+
+    public Member(String name, String email, String nickname, Password password) {
+        this.name = name;
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+    }
+
+    public boolean checkPassword(String password) {
+        return this.password.isMatch(password);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(getId(), member.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
