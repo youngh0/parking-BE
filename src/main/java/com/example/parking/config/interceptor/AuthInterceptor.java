df0e7ed6b7e97ececaf3a12f55f9bbbca3f0b5cc
JunDonghyuk <ehdgur4814@naver.com>
2024-02-05 00:19:54 +0900

    feat: AuthInterceptor 구현

---
+++ src/main/java/com/example/parking/config/interceptor/AuthInterceptor.java
@@ -0,0 +1,26 @@
+package com.example.parking.config.interceptor;
+
+import com.example.parking.auth.AuthService;
+import com.example.parking.auth.MemberSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@RequiredArgsConstructor
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private static final String JSESSIONID = "JSESSIONID";
+
+    private final AuthService authService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String sessionId = request.getHeader(JSESSIONID);
+        MemberSession session = authService.findSession(sessionId);
+        authService.updateSessionExpiredAt(session);
+        return true;
+    }
+}
