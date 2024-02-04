6d2c282f0c24d8f10989f4770a29d99ca585a881
JunDonghyuk <ehdgur4814@naver.com>
2024-02-04 21:04:41 +0900

    feat: 패스워드 암호화 구현

---
+++ src/main/java/com/example/parking/util/cipher/EncryptionException.java
@@ -0,0 +1,8 @@
+package com.example.parking.util.cipher;
+
+public class EncryptionException extends RuntimeException {
+
+    public EncryptionException(String message) {
+        super(message);
+    }
+}
