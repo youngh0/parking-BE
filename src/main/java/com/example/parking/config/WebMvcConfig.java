da37fced22e8a43dc28009b6cd78e63eef0222a1
JunDonghyuk <ehdgur4814@naver.com>
2024-02-05 00:20:41 +0900

    feat: WebMvcConfig 구현

---
+++ src/main/java/com/example/parking/config/WebMvcConfig.java
@@ -0,0 +1,34 @@
+package com.example.parking.config;
+
+import com.example.parking.config.argumentresolver.AuthArgumentResolver;
+import com.example.parking.config.interceptor.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+    private final AuthArgumentResolver authArgumentResolver;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(List.of(
+                        "/users",
+                        "/login"
+                ));
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authArgumentResolver);
+    }
+}
