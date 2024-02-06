ba3c449f33a4fe9990371fd6858e4da1599dda39
Youngho <cyh6099@gmail.com>
2024-02-06 17:04:22 +0900

    feat: 주차장 목록 조회 쿼리 파라미터 전용 argument resolver 구현

---
+++ src/main/java/com/example/parking/config/argumentresolver/parking/ParkingQueryArgumentResolver.java
@@ -0,0 +1,36 @@
+package com.example.parking.config.argumentresolver.parking;
+
+import com.example.parking.application.parking.dto.ParkingQueryRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class ParkingQueryArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(ParkingQuery.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        return new ParkingQueryRequest(
+                webRequest.getParameter("operationType"),
+                webRequest.getParameter("parkingType"),
+                webRequest.getParameter("feeType"),
+                Boolean.parseBoolean(webRequest.getParameter("cardEnabled")),
+                webRequest.getParameter("priority"),
+                Integer.parseInt(webRequest.getParameter("hours")),
+                Double.valueOf(webRequest.getParameter("latitude")),
+                Double.valueOf(webRequest.getParameter("longitude")),
+                Integer.parseInt(webRequest.getParameter("radius"))
+        );
+    }
+}
