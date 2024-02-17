cd9981917e24c7dc7f0bcc70a71ef7eb7d3a0264
This2sho <zx00018@naver.com>
2024-02-17 13:58:23 +0900

    refactor: RequiredArgsConstructor 사용하도록 변경

--- src/main/java/com/example/parking/api/searchcondition/SearchConditionController.java
+++ src/main/java/com/example/parking/api/searchcondition/SearchConditionController.java
@@ -3,20 +3,18 @@ package com.example.parking.api.searchcondition;
 import com.example.parking.application.searchcondition.SearchConditionService;
 import com.example.parking.application.searchcondition.dto.SearchConditionDto;
 import com.example.parking.config.argumentresolver.MemberAuth;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class SearchConditionController {
 
     private final SearchConditionService searchConditionService;
-
-    public SearchConditionController(SearchConditionService searchConditionService) {
-        this.searchConditionService = searchConditionService;
-    }
 
     @GetMapping("/users/parkings/options")
     public ResponseEntity<SearchConditionDto> loadSearchCondition(@MemberAuth Long memberId) {
