383217b620ee4da601c8492537580ba691042772
Youngho <cyh6099@gmail.com>
2024-02-21 13:52:22 +0900

    feat: 인증 코드 엔티티 정의

---
+++ src/main/java/com/example/parking/auth/AuthCode.java
@@ -0,0 +1,35 @@
+package com.example.parking.auth;
+
+import com.example.parking.domain.AuditingEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class AuthCode extends AuditingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String destination;
+
+    private String authCode;
+
+    @Enumerated(EnumType.STRING)
+    private AuthCodeType authCodeType;
+
+    private Boolean isUsed = Boolean.FALSE;
+
+    public AuthCode(String destination, String authCode, AuthCodeType authCodeType) {
+        this.destination = destination;
+        this.authCode = authCode;
+        this.authCodeType = authCodeType;
+    }
+}
