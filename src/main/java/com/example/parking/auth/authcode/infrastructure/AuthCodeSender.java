e3771c3c077a04df5c0b559b2c91882a2fbd80f3
Youngho <cyh6099@gmail.com>
2024-02-27 02:37:08 +0900

    refactor: public 제거

--- src/main/java/com/example/parking/auth/authcode/infrastructure/AuthCodeSender.java
+++ src/main/java/com/example/parking/auth/authcode/infrastructure/AuthCodeSender.java
@@ -2,5 +2,5 @@ package com.example.parking.auth.authcode.infrastructure;
 
 public interface AuthCodeSender {
 
-    public void send(String destination, String authCode);
+    void send(String destination, String authCode);
 }
