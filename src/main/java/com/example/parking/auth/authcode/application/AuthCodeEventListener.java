c5019c8311c3ce64e1ee5a8a0e6b7668825901bd
Youngho <cyh6099@gmail.com>
2024-03-02 01:43:41 +0900

    feat: 인증 코드 생성 시 5분 뒤 삭제 스케줄링 구현

--- src/main/java/com/example/parking/auth/authcode/application/AuthCodeEventListener.java
+++ src/main/java/com/example/parking/auth/authcode/application/AuthCodeEventListener.java
@@ -3,7 +3,12 @@ package com.example.parking.auth.authcode.application;
 
 import com.example.parking.auth.authcode.event.AuthCodeCreateEvent;
 import com.example.parking.auth.authcode.infrastructure.AuthCodeSender;
+import java.time.Instant;
+import java.util.StringJoiner;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -12,12 +17,37 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Component
 public class AuthCodeEventListener {
 
+    @Value("${authcode.expired-time}")
+    private Integer authCodeExpired;
+
     private final AuthCodeSender authCodeSender;
+    private final TaskScheduler taskScheduler;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void sendAuthCode(AuthCodeCreateEvent authCodeCreateEvent) {
         String destination = authCodeCreateEvent.getDestination();
         String authCode = authCodeCreateEvent.getAuthCode();
         authCodeSender.send(destination, authCode);
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void scheduledAuthCodeRemove(AuthCodeCreateEvent authCodeCreateEvent) {
+        String destination = authCodeCreateEvent.getDestination();
+        String authCodePlatform = authCodeCreateEvent.getAuthCodePlatform();
+        String authCodeCategory = authCodeCreateEvent.getAuthCodeCategory();
+
+        String authCodeKey = generateAuthCodeKey(destination, authCodePlatform, authCodeCategory);
+        taskScheduler.schedule(() -> redisTemplate.delete(authCodeKey), Instant.now().plusSeconds(authCodeExpired));
+    }
+
+    private String generateAuthCodeKey(String destination, String authCodePlatform,
+                                       String authCodeCategory) {
+        StringJoiner stringJoiner = new StringJoiner(":");
+        return stringJoiner
+                .add(destination)
+                .add(authCodePlatform)
+                .add(authCodeCategory)
+                .toString();
     }
 }
