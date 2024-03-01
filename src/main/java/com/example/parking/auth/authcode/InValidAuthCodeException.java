c4a678344c6da4205e2411e0ebd66a01bd89039b
Youngho <cyh6099@gmail.com>
2024-03-02 02:26:37 +0900

    feat: super() 추가

--- src/main/java/com/example/parking/auth/authcode/InValidAuthCodeException.java
+++ src/main/java/com/example/parking/auth/authcode/InValidAuthCodeException.java
@@ -5,6 +5,7 @@ public class InValidAuthCodeException extends RuntimeException {
     private final String message;
 
     public InValidAuthCodeException(String message) {
+        super(message);
         this.message = message;
     }
 }
