007dc2c439e48d3241c2214c85c08762e84ef1ef
Youngho <cyh6099@gmail.com>
2024-03-02 01:43:18 +0900

    feat: 이벤트 페이로드 수정

--- src/main/java/com/example/parking/auth/authcode/event/AuthCodeCreateEvent.java
+++ src/main/java/com/example/parking/auth/authcode/event/AuthCodeCreateEvent.java
@@ -1,20 +1,19 @@
 package com.example.parking.auth.authcode.event;
 
+import lombok.Getter;
+
+@Getter
 public class AuthCodeCreateEvent {
 
     private final String destination;
     private final String authCode;
+    private final String authCodePlatform;
+    private final String authCodeCategory;
 
-    public AuthCodeCreateEvent(String destination, String authCode) {
+    public AuthCodeCreateEvent(String destination, String authCode, String authCodePlatform, String authCodeCategory) {
         this.destination = destination;
         this.authCode = authCode;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public String getAuthCode() {
-        return authCode;
+        this.authCodePlatform = authCodePlatform;
+        this.authCodeCategory = authCodeCategory;
     }
 }
