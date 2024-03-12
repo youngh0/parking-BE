da3fd6001e3d3f186acdcee171a1506c7e22056b
Youngho <cyh6099@gmail.com>
2024-03-12 22:13:10 +0900

    refactor: 주석제거

--- src/test/java/com/example/parking/container/MySQLDataCleaner.java
+++ src/test/java/com/example/parking/container/MySQLDataCleaner.java
@@ -1,13 +1,12 @@
 package com.example.parking.container;
 
 import jakarta.persistence.EntityManager;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.shaded.com.google.common.base.CaseFormat;
-
-import java.util.List;
 
 @Component
 public class MySQLDataCleaner {
@@ -24,11 +23,6 @@ public class MySQLDataCleaner {
                 .map(table -> CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, table.getName()))
                 .toList();
 
-//        jdbcTemplate.execute("TRUNCATE TABLE auth_code");
-//        jdbcTemplate.execute("TRUNCATE TABLE parking");
-//        jdbcTemplate.execute("TRUNCATE TABLE member_session");
-//        jdbcTemplate.execute("TRUNCATE TABLE search_condition");
-//        jdbcTemplate.execute("TRUNCATE TABLE member");
         setForeignKeyEnabled(false);
         tableNames.forEach(table -> jdbcTemplate.execute("TRUNCATE table " + table));
         setForeignKeyEnabled(true);
