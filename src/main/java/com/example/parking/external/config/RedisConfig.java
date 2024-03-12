52425ccc6f5a00f44385e15fd01fc9a9890aefdc
This2sho <zx00018@naver.com>
2024-03-13 05:05:59 +0900

    fix: 빈 설정 수정

--- src/main/java/com/example/parking/external/config/RedisConfig.java
+++ src/main/java/com/example/parking/external/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.example.parking.external.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -22,6 +23,7 @@ public class RedisConfig {
         return new LettuceConnectionFactory(host, port);
     }
 
+    @Primary
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
