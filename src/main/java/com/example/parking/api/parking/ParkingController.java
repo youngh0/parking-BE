45287100eda4454181cc31bb5872ab24fd90c709
Youngho <cyh6099@gmail.com>
2024-03-29 14:06:01 +0900

    feat: swagger 추가

--- src/main/java/com/example/parking/api/parking/ParkingController.java
+++ src/main/java/com/example/parking/api/parking/ParkingController.java
@@ -7,23 +7,28 @@ import com.example.parking.application.parking.dto.ParkingSearchConditionRequest
 import com.example.parking.config.argumentresolver.MemberAuth;
 import com.example.parking.config.argumentresolver.parking.ParkingQuery;
 import com.example.parking.config.argumentresolver.parking.ParkingSearchCondition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "주차장 컨트롤러")
 @RequiredArgsConstructor
 @RestController
 public class ParkingController {
 
     private final ParkingService parkingService;
 
+    @Operation(summary = "주차장 반경 조회", description = "주차장 반경 조회")
     @GetMapping("/parkings")
     public ResponseEntity<ParkingLotsResponse> find(
             @ParkingQuery ParkingQueryRequest parkingQueryRequest,
             @ParkingSearchCondition ParkingSearchConditionRequest parkingSearchConditionRequest,
-            @MemberAuth(nullable = true) Long parkingMemberId
+            @Parameter(hidden = true) @MemberAuth(nullable = true) Long parkingMemberId
     ) {
         ParkingLotsResponse parkingLots = parkingService.findParkingLots(parkingQueryRequest,
                 parkingSearchConditionRequest, parkingMemberId, LocalDateTime.now());
