44e835c4ee5d32f999b0a79d9d74cffb2c684dc6
Youngho <cyh6099@gmail.com>
2024-04-02 00:22:33 +0900

    feat: swagger 적용

--- src/main/java/com/example/parking/api/parking/ParkingController.java
+++ src/main/java/com/example/parking/api/parking/ParkingController.java
@@ -2,6 +2,8 @@ package com.example.parking.api.parking;
 
 import com.example.parking.application.parking.ParkingService;
 import com.example.parking.application.parking.dto.ParkingDetailInfoResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "주차장 컨트롤러")
 @RequiredArgsConstructor
 @RestController
 public class ParkingController {
 
     private final ParkingService parkingService;
 
+    @Operation(summary = "주차장 상세조회", description = "주차장 상세조회")
     @GetMapping("/parkings/{parkingId}")
     public ResponseEntity<ParkingDetailInfoResponse> findParking(@PathVariable Long parkingId) {
         ParkingDetailInfoResponse parkingDetailInfoResponse = parkingService.findParking(parkingId,
