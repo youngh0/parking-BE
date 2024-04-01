03dcdd321b23246b875ec0b734d58887c0a7ce1a
This2sho <zx00018@naver.com>
2024-04-01 18:16:36 +0900

    fix: 잘못 사용되고 있는 값 객체 제거

--- src/test/java/com/example/parking/fake/FakeFavoriteRepository.java
+++ src/test/java/com/example/parking/fake/FakeFavoriteRepository.java
@@ -3,18 +3,19 @@ package com.example.parking.fake;
 import com.example.parking.domain.favorite.Favorite;
 import com.example.parking.domain.favorite.FavoriteRepository;
 import com.example.parking.domain.member.Member;
-import com.example.parking.domain.parking.ParkingId;
+import com.example.parking.domain.parking.Parking;
 import com.example.parking.support.Association;
 import java.util.List;
 
 public class FakeFavoriteRepository implements FavoriteRepository {
+
     @Override
     public Favorite save(Favorite favorite) {
         return null;
     }
 
     @Override
-    public void deleteByMemberIdAndParkingId(Association<Member> memberId, Association<ParkingId> parkingId) {
+    public void deleteByMemberIdAndParkingId(Association<Member> memberId, Association<Parking> parkingId) {
 
     }
 
