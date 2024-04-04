8c10ab0f1305ed03441846eb9b8aef23b7515432
This2sho <zx00018@naver.com>
2024-04-04 18:01:32 +0900

    fix: 서버 시간대 한국으로 설정

--- src/main/java/com/example/parking/ParkingApplication.java
+++ src/main/java/com/example/parking/ParkingApplication.java
@@ -1,5 +1,7 @@
 package com.example.parking;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +11,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class ParkingApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ParkingApplication.class, args);
